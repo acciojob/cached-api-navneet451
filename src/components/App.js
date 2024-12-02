317c51e955aa76872c7bafcba4a9c206ad92681e
Navneet_thakur <127828104+navneet451@users.noreply.github.com>
2024-12-02 11:32:45 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,22 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [userId, setUserId] = useState(1);
+  const handleChangeUserId=(e)=>{
+    setUserId(Number.(e.target.value));
+  };
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Post Fetcher App</h1>
+      <input 
+        type="number" 
+        value={userId} 
+        onChange={handleChangeUserId} 
+        placeholder="Enter User ID"
+      />
+      <PostFetcher userId={userId} />
     </div>
-  )
+  );
 }
 
 export default App
